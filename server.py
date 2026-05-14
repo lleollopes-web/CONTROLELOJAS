451bdcad7ee59eca9c9db0cc54954ee002dfa5e3
lleollopes-web <leandro@engevias.eng.br>
2026-05-14 12:09:48 -0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,15 +1,17 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, Response
 import os
 
-app = Flask(__name__, static_folder='.')
+app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'dashboard.html')
+    return send_from_directory(BASE_DIR, 'painel.html')
 
 @app.route('/<path:filename>')
 def serve_file(filename):
-    return send_from_directory('.', filename)
+    return send_from_directory(BASE_DIR, filename)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
