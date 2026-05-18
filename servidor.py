fdfcd88ffb678272ed5f78b2268b314a4e0c3f7c
lleollopes-web <leandro@engevias.eng.br>
2026-05-18 13:35:41 -0300

    Add files via upload

--- servidor.py
+++ servidor.py
@@ -1,8 +1,9 @@
-from flask import Flask, send_from_directory, abort
+from flask import Flask, send_from_directory, abort, Response
 import os
 
 app = Flask(__name__)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+GH_TOKEN = os.environ.get('GH_TOKEN', '')
 
 def find_html():
     for name in ['painel.html', 'dashboard.html', 'index.html']:
@@ -12,11 +13,19 @@ def find_html():
 
 @app.route('/')
 def index():
-    html = find_html()
-    if html:
-        return send_from_directory(BASE_DIR, html)
-    files = os.listdir(BASE_DIR)
-    return '<h2>Arquivos encontrados:</h2><ul>' + ''.join(f'<li>{f}</li>' for f in sorted(files)) + '</ul>'
+    html_name = find_html()
+    if not html_name:
+        files = os.listdir(BASE_DIR)
+        return '<h2>Arquivos encontrados:</h2><ul>' + ''.join(f'<li>{f}</li>' for f in sorted(files)) + '</ul>'
+
+    with open(os.path.join(BASE_DIR, html_name), 'r', encoding='utf-8') as f:
+        html = f.read()
+
+    # Injeta o token como variável JS antes do </head>
+    inject = f'<script>window.__GH_TOKEN__="{GH_TOKEN}";</script>'
+    html = html.replace('</head>', inject + '</head>', 1)
+
+    return Response(html, mimetype='text/html')
 
 @app.route('/<path:filename>')
 def serve_file(filename):
